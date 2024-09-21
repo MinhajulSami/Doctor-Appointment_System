23ab0ae2fef17a0e24fa57d6fa08efbca37b79cc
Minhajul Sami <2021-2-60-020@std.ewubd.edu>
2024-09-21 15:55:56 +0600

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.length("*", function(req, res){
+app.get("*", function(req, res){
 res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 //port
